1a0a00114e0d4ddca7a1238088d79261e123b114
noplur <geoduckphoto@gmail.com>
2021-01-11 20:48:18 -0800

    list issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,27 +3,43 @@ var issueContainerEl = document.querySelector("#issues-container");
 // DOM reference to "limit-warning" container
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+    
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    // fetch request
-    fetch(apiUrl).then(function(response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-              displayIssues(data);
-          
-              // check if api has paginated issues
-              if (response.headers.get("Link")) {
-                displayWarning(repo)
-              }
-            });
-          }
-          else {
-            console.log(response);
-            alert("There was a problem with your request!");
-          }
-        });
+    // make a get request to url
+fetch(apiUrl).then(function(response) {
+  // request was successful
+  if (response.ok) {
+    response.json().then(function(data) {
+      displayIssues(data);
+
+      // check if api has paginated issues
+      if (response.headers.get("Link")) {
+        displayWarning(repo);
+      }
+    });
+  } else {
+    // if not successful, redirect to homepage
+    document.location.replace("./index.html");
+  }
+});
       };
 
 var displayIssues = function(issues) {
@@ -79,4 +95,4 @@ issueContainerEl.appendChild(issueEl);
     limitWarningEl.appendChild(linkEl);
     };
 
-  getRepoIssues("facebook/react");
+  getRepoName();
